cb2a758367fafe7e06a9491be50e202af17bbab0
xodnjs <xodnjs@xodnjsui-MacBookPro.local>
2024-11-21 20:00:06 +0900

    codeup [Basic - Repeated Execution Structure]

---
+++ codeup/Algorithm기초 100제/Basic - Repeated Execution Structure/1073.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(void)
+{
+    int n;
+    while(true)
+    {
+        cin>>n;
+        if(n==0)
+        {
+            break;
+        }
+        cout<<n<<endl;
+    }
+    return 0;
+}
---
+++ codeup/Algorithm기초 100제/Basic - Repeated Execution Structure/1074.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(void)
+{
+    int n;
+    cin>>n;
+    for(int i=n;i>0;i--)
+    {
+        cout<<i<<endl;
+    }
+    return 0;
+}
