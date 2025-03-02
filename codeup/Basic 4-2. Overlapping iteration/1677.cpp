85df6bd095fe85d8e83711cca1eabd96b9c07175
xodnjsqkr <taewon0757@gmail.com>
2025-03-02 11:26:13 +0900

    codeup [Basic 4-2. Overlapping iteration]

--- codeup/Basic 4-2. Overlapping iteration/1677.cpp
+++ codeup/Basic 4-2. Overlapping iteration/1677.cpp
@@ -4,6 +4,28 @@ int main(void)
 {
     int x,y;
     cin>>x>>y;
-    
+    for(int i=0;i<y;i++)
+    {
+        for(int j=0;j<x;j++)
+        {
+            if((i==0||i==y-1)&&(j==0||j==x-1))
+            {
+                cout<<"+";
+            }
+            else if(i==0||i==y-1)
+            {
+                cout<<"-";
+            }
+            else if(j==0||j==x-1)
+            {
+                cout<<"|";
+            }
+            else
+            {
+                cout<<" ";
+            }
+        }
+        cout<<"\n";
+    }
     return 0;
 }
